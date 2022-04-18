257e6191eea1dfefa061fb70fc976ed0cf5a17c1
JWViking <julianawilcox@gmail.com>
2022-04-18 13:10:32 -0600

    curly bracket error fixed

--- models/User.js
+++ models/User.js
@@ -49,29 +49,20 @@ User.init(
           }
         }
       },
-    {
+      {
         hooks: {
-            // set up beforeCreate lifecycle "hook" functionality
-            async beforeCreate(newUserData) {
-              newUserData.password = await bcrypt.hash(newUserData.password, 10);
-              return newUserData;
-            },
-        }
-    },
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize: sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'User'
-  }
+          // set up beforeCreate lifecycle "hook" functionality
+          beforeCreate: async (newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+          }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User'
+      }
 );
 
 module.exports = User;
